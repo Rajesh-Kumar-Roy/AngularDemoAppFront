bf609c574623787e8e677fd8f217af29f8d6209e
Rajesh-Kumar-Roy <rajeshkumarroy01998@gmail.com>
2020-10-05 15:59:36 +0600

    solve total pirce problem and clean

--- shop-app/src/app/sales-master-details/sales-master-details.component.ts
+++ shop-app/src/app/sales-master-details/sales-master-details.component.ts
@@ -1,3 +1,4 @@
+import { SalesDetails } from './../Model/SaleDetails';
 import { Customer } from './../Model/Customer';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
@@ -8,17 +9,16 @@ import { __values } from 'tslib';
 import { ProductTypeService } from '../service/product-type.service';
 import { ProductType } from '../Model/productType';
 import { SalesDetailsService } from '../service/sales-details.service';
-import { SalesDetails } from '../Model/SaleDetails';
 import { SalesService } from '../service/sales.service';
 import { CustomerService } from '../service/customer.service';
 import { ToastrService } from 'ngx-toastr';
-import { Console } from 'console';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-sales-master-details',
   templateUrl: './sales-master-details.component.html',
-  styleUrls: ['./sales-master-details.component.css']
+  styleUrls: ['./sales-master-details.component.css'],
 })
 export class SalesMasterDetailsComponent implements OnInit {
   public $Model: Sales = null;
@@ -35,13 +35,12 @@ export class SalesMasterDetailsComponent implements OnInit {
   productList: Array<Product> = [];
   productType: Array<ProductType> = [];
   result: any;
+  priceArray: Array<number> = [];
   totalPrices: Array<number> = [];
   customerCodeValue: any = '';
   customer: Customer[];
   loadSaleCode: any;
   success = false;
-  @Input() untPrce: number;
-  @Input() tolprc: number;
   pdTypeId: number;
   csId: number;
   constructor(
@@ -51,15 +50,17 @@ export class SalesMasterDetailsComponent implements OnInit {
     private productService: ProductServiceService,
     private productTypeService: ProductTypeService,
     private salesDetailsService: SalesDetailsService,
-    private toastr: ToastrService
+    private router: Router,
+    private toastr: ToastrService,
   ) {
     this.$Model = new Sales(); this.$DetailModel = new SalesDetails();
   }
 
-
   ngOnInit(): void {
     this.salesForm = this.fb.group({
       id: [this.$Model.id],
+      customerName: [''],
+      customerCode: [''],
       customerId: [this.csId, Validators.required],
       date: [this.$Model.date, Validators.required],
       saleNo: [this.$Model.saleNo],
@@ -99,13 +100,14 @@ export class SalesMasterDetailsComponent implements OnInit {
       }
     });
     this.productService.getAllisDeleteFase().subscribe(res => this.productList = res);
-
   }
+
+
   get Sel(): any {
     return this.salesForm.controls;
   }
-   // tslint:disable-next-line: typedef
-   get salesDetail() {
+  // tslint:disable-next-line: typedef
+  get salesDetail() {
     return this.salesForm.get('salesDetails') as FormArray;
   }
   addDetailsFormGroup(): FormGroup {
@@ -121,17 +123,17 @@ export class SalesMasterDetailsComponent implements OnInit {
 
   }
 
- // tslint:disable-next-line: typedef
- private newDetailsFormGroup(){
-    return new FormGroup({
-      productTypeId: new FormControl(null),
-      productId: new FormControl(null),
-      unitPrice: new FormControl(null),
-      qty: new FormControl(null),
-      totalPrice: new FormControl(null),
-      description: new FormControl(''),
-    });
-  }
+  // tslint:disable-next-line: typedef
+  //  private newDetailsFormGroup(){
+  //     return new FormGroup({
+  //       productTypeId: new FormControl(null),
+  //       productId: new FormControl(null),
+  //       unitPrice: new FormControl(null),
+  //       qty: new FormControl(null),
+  //       totalPrice: new FormControl(null),
+  //       description: new FormControl(''),
+  //     });
+  //   }
   // registration Button CLick
   registrationButtonClick(): void {
     this.regShowHide = true;
@@ -142,7 +144,7 @@ export class SalesMasterDetailsComponent implements OnInit {
     this.regShowHide = false;
     this.hideCustomerCode = true;
   }
-
+// remove Detail
   removeDetailsButton(detailsIndex: number): void {
     const detailsFormArray = (this.salesForm.get('salesDetails') as FormArray);
     detailsFormArray.removeAt(detailsIndex);
@@ -150,40 +152,24 @@ export class SalesMasterDetailsComponent implements OnInit {
     detailsFormArray.markAsUntouched();
   }
 
-  // changeProductSelectValue($event): void {
-  //   const pId = $event.target.value;
-  //   const plen = this.products.length;
-  //   for (let i = 0; i < plen; i++) {
-  //     const produ = this.products[i];
-  //     // tslint:disable-next-line: triple-equals
-  //     if (produ.id == pId) {
-  //       this.unitPriceByProductId.push(produ.price) ;
-  //     }
-  //   }
-  //   console.log(this.unitPriceByProductId);
-  // }
-
 
   // get  price by product id
-  getProductUnitPrice(inputId: number): Product[]{
+  getProductUnitPrice(inputId: number): Product[] {
+    // tslint:disable-next-line: triple-equals
     return this.productList.filter(item => item.id == inputId);
   }
 
-    // get qty
-    onKey(event, n: number): any {
-      this.totalPrices.push(1200 * event.target.value);
-        // this.totalPrices.push (event.target.value * Number(this.getProductUnitPrice(n)));
-    }
   // product dropdown value set
   getProduct(inputId: number): Product[] {
+    // tslint:disable-next-line: triple-equals
     return this.productList.filter(item => item.productTypeId == inputId);
   }
-
-  getTotalPrice(n: number): number{
-    for (let i = 0; i < n + 1; i++ ){
-        return this.totalPrices[i];
-    }
+  // get Total Price
+  getTotalPrice(qty: number, unitPrice: number): number {
+    return qty * unitPrice;
   }
+
+
   // Find Customer Name and Sales  Code
   findCustomer(event): void {
     // tslint:disable-next-line: triple-equals
@@ -202,6 +188,9 @@ export class SalesMasterDetailsComponent implements OnInit {
         if (pot.customerCode === codeUpper) {
           this.customerCodeValue = pot.firstName;
           this.csId = pot.id;
+          this.salesForm.patchValue({
+            customerId: pot.id
+          });
 
         }
       }
@@ -209,20 +198,21 @@ export class SalesMasterDetailsComponent implements OnInit {
       // tslint:disable-next-line: triple-equals
       else if (('013' == codeCheck || '014' === codeCheck || '015' === codeCheck || '016' === codeCheck || '017' === codeCheck ||
         '019' === codeCheck || '018' === codeCheck) && check.length === 11) {
-        console.log(check.length);
         if (pot.mobileNo === check) {
           this.customerCodeValue = pot.firstName;
           this.csId = pot.id;
-
+          this.salesForm.patchValue({
+            customerId: pot.id
+          });
         }
         if (pot.mobileNo !== check && len === 11) {
-          this.toastr.warning('Can Not Find Customer', 'Alert', {
+          this.toastr.success('Find Customer', 'customer', {
             timeOut: 1500
           });
         }
       }
       else if (len === 19) {
-        this.toastr.warning('Can Not Find Customer', 'Alert', {
+        this.toastr.success('Find Customer', 'Customer', {
           timeOut: 1500
         });
       }
@@ -235,29 +225,47 @@ export class SalesMasterDetailsComponent implements OnInit {
 
   // Save Value
   onSaveSale(): any {
-    console.log(this.salesForm.value);
     this.submitted = true;
-    if (this.salesForm.invalid)
-    {
+    console.log(this.salesForm.value);
+    if (this.salesForm.invalid) {
       this.toastr.error('Fill the Form Correctly!', 'Sale');
       return;
     }
     this.saleService.create(this.salesForm.value).subscribe(res => {
       if (res) {
         this.toastr.success('Save successfully', 'Sale');
-        this.salesForm.reset();
-        this.submitted = false;
+        this.salesForm.patchValue({
+          id: 0,
+          customerId: 0,
+          customerName: '',
+          customerCode: '',
+          date: new Date(),
+          description: '',
+        });
+        this.salesDetail.clear();
+        this.addProduct();
+        this.clean();
       }
-
     });
+  }
+
+  clean(): void {
+    if (this.salesForm.value.saleNo == '' || this.salesForm.value.saleNo == null) {
+      // get sale code when page reload
+      this.saleService.getSaleCode().subscribe(res => {
+        if (res?.length > 0) {
+          this.loadSaleCode = res;
+        }
+      });
+      console.log();
+    }
+    this.submitted = false;
   }
 
   addProduct(): void {
     this.productAdded = true;
     const ctrl = (this.salesForm.get('salesDetails') as FormArray);
-    ctrl.push(this.newDetailsFormGroup());
+    ctrl.push(this.addDetailsFormGroup());
   }
-
-
 
 }
